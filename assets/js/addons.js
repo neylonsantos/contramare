0e3419dc304802c7c262256ea7df309cb2e3d405
Neylon Santos <neylonssantos@gmail.com>
2022-01-24 16:05:53 -0300

    Finishing blog page

--- assets/js/addons.js
+++ assets/js/addons.js
@@ -3,3 +3,11 @@ function show() {
   var element = document.getElementById("menu");
   element.classList.toggle("show");
 }
+
+// Popover
+document.addEventListener("DOMContentLoaded", function(){
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function(element){
+      return new bootstrap.Popover(element);
+  });
+});
