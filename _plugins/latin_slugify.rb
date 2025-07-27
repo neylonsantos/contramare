bad7c0a53a63ed1f8c78412e1cac02313d4b488c
Neylon Santos <neylonssantos@gmail.com>
2025-07-27 02:37:23 -0300

    up changes

--- _plugins/latin_slugify.rb
+++ _plugins/latin_slugify.rb
@@ -1,14 +1,11 @@
-Jekyll::Hooks.register :posts, :pre_render do |post|
-  # Aplica slugify com mode latin para remover acentos das categorias nas URLs
-  if post.data['categories']
-    categories = [post.data['categories']].flatten
-    slug_categories = categories.map { |cat| Jekyll::Utils.slugify(cat, mode: 'latin') }
-
-    # Define o permalink usando categorias sem acentos
-    if post.data['permalink'].nil?
-      category_slug = slug_categories.first || 'blog'
-      title_slug = Jekyll::Utils.slugify(post.data['title'], mode: 'latin')
-      post.data['permalink'] = "/#{category_slug}/#{title_slug}/"
+module Jekyll
+  module Drops
+    class UrlDrop
+      def latin_slugified_categories
+        Array(@obj.data["categories"]).each_with_object(Set.new) do |category, set|
+          set << Jekyll::Utils.slugify(category.to_s, mode: 'latin')
+        end.to_a.join("/")
+      end
     end
   end
 end
